bb5d420adef4e3e4a2646fad2154692d156f465e
ST10476443 <ST10476443@imconnect.edu.za>
2026-03-31 13:30:45 +0200

    Add project files.

---
+++ CyberSecurityChatbot/Program.cs
@@ -0,0 +1,31 @@
+﻿using CybersecurityChatbot;
+using System;
+using System.Threading;
+
+namespace CybersecurityAwarenessChatbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cybersecurity Awareness Chatbot";
+            //emoji
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Display ASCII Logo
+            AsciiArt.DisplayLogo();
+
+            // Play Voice Greeting
+            AudioPlayer.PlayGreeting();
+
+            Thread.Sleep(500);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome to the Cybersecurity Awareness Bot!");
+            Console.ResetColor();
+
+            // Start Chatbot
+            Chatbot bot = new Chatbot();
+            bot.Start();
+        }
+    }
+}
